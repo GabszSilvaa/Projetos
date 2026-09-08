80bedfd4c069b514e95638dd006a27ae9f5aa365
Gabriel Silva <gs8428211@gmail.com>
2026-09-08 11:40:59 -0300

    Add files via upload

---
+++ SistemadeRestaurante.java
@@ -0,0 +1,14 @@
+package App;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Locale;
+public class SistemadeRestaurante {
+    public static <Produto> void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        System.out.println("===Bem-vindo ao Mentes binários de Restaurante!===");
+        ArrayList<Produto> produtos = new ArrayList<>();
+        
+        }
+    }
+
